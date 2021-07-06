b0b328f74339f4d6b405fc5be406b889780ce907
ellisPae <ellis.h.pae@gmail.com>
2021-07-06 12:32:52 -0400

    Implement calculating functions

--- calculator.jsx
+++ calculator.jsx
@@ -3,16 +3,67 @@ import React from 'react';
 class Calculator extends React.Component{
   constructor(props){
     super(props);
-    //your code here
+    this.state = {num1: "", num2: "", result: 0}
+    this.setNum1 = this.setNum1.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
-  //your code here
+  setNum1(e) {
+    const num1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num1 });
+  }
 
-  render(){
+  setNum2(e) {
+    const num2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2 });
+  }
+
+  add(e) {
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result });
+  }
+
+  subtract(e) {
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({ result });
+  }
+
+  multiply(e) {
+    e.preventDefault();
+    const result = this.state.num1 * this.state.num2;
+    this.setState({ result });
+  }
+
+  divide(e) {
+    e.preventDefault();
+    const result = this.state.num1 / this.state.num2;
+    this.setState({ result });
+  }
+
+
+  clear(e) {
+    e.preventDefault();
+    this.setState({ num1: "", num2: "", result: 0 });
+  }
+  
+
+  render() {
+    const {num1, num2, result} = this.state;
+
     return (
       <div>
-        <h1>Hello World</h1>
-        //your code will replace this
+        <h1>{result}</h1>
+
+        <input onChange={this.setNum1} value={num1}/>
+        <input onChange={this.setNum2} value={num2}/>
+        <button onClick={this.clear}>Clear</button>
+
       </div>
     );
   }
